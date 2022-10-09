a0d3b8d0c8bd60d141a749a19660da29ed659b4a
gal0wsky <maciej.gawlowski.biznes@gmail.com>
2022-10-09 18:40:36 +0200

    Adding SSL certificate signed by local CA; adding Docker support with HTTPS --> 09.10.2022

--- findaroundAPI/Program.cs
+++ findaroundAPI/Program.cs
@@ -7,6 +7,31 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+HostCertConfig.CertPath = Environment.ExpandEnvironmentVariables(@builder.Configuration["CertPath"]);
+HostCertConfig.CertPass = Environment.ExpandEnvironmentVariables(@builder.Configuration["CertPassword"]);
+
+//builder.Host.ConfigureWebHostDefaults(webBuilder =>
+//{
+//    webBuilder.ConfigureKestrel(opt =>
+//    {
+//        opt.ListenAnyIP(80);
+
+//        opt.ListenAnyIP(443, listOpt =>
+//        {
+//            listOpt.UseHttps(HostCertConfig.CertPath, HostCertConfig.CertPass);
+//        });
+//    });
+//});
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80);
+    options.ListenAnyIP(443, listOpt =>
+    {
+        listOpt.UseHttps(HostCertConfig.CertPath, HostCertConfig.CertPass);
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,3 +49,8 @@ app.MapControllers();
 
 app.Run();
 
+public static class HostCertConfig
+{
+    public static string CertPath { get; set; }
+    public static string CertPass { get; set; }
+}
