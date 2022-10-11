19b0861643a949622ffe8d7dfe992ab109da91ec
gal0wsky <maciej.gawlowski.biznes@gmail.com>
2022-10-11 14:25:14 +0200

    findaroundShared.Models.Post contains Category and Status properties, editing ModelsMappingProfile.cs --> 11.10.2022

--- findaroundAPI/MappingProfiles/ModelsMappingProfile.cs
+++ findaroundAPI/MappingProfiles/ModelsMappingProfile.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using findaroundAPI.Entities;
 using findaroundShared.Models;
+using findaroundAPI.Helpers;
 
 namespace findaroundAPI.MappingProfiles
 {
@@ -17,7 +18,9 @@ namespace findaroundAPI.MappingProfiles
 				.ForMember(e => e.Images, mp => mp.MapFrom(p => p.Images.Select(x => new PostsImagesEntity()
 				{
 					Image = x
-				})));
+				})))
+				.ForMember(e => e.Status, mp => mp.MapFrom(p => p.Status.ToString()))
+                .ForMember(e => e.Category, mp => mp.MapFrom(p => p.Category.ToString()));
 
 
 			CreateMap<PostEntity, Post>()
@@ -26,7 +29,9 @@ namespace findaroundAPI.MappingProfiles
 					Latitude = e.Latitude,
 					Longitute = e.Longitude
 				}))
-				.ForMember(p => p.Images, mp => mp.MapFrom(e => e.Images.Select(x => new string(x.Image))));
+				.ForMember(p => p.Images, mp => mp.MapFrom(e => e.Images.Select(x => new string(x.Image))))
+				.ForMember(p => p.Status, mp => mp.MapFrom(e => EnumHelpers.ToPostStatus(e.Status)))
+                .ForMember(p => p.Category, mp => mp.MapFrom(e => EnumHelpers.ToPostCategory(e.Category)));
 		}
 	}
 }
--- findaroundShared/Models/Post.cs
+++ findaroundShared/Models/Post.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace findaroundShared.Models
 {
 	public class Post
@@ -6,7 +8,9 @@ namespace findaroundShared.Models
 		public int Id { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
-		public PostLocation Location { get; set; }
+        public PostStatus Status { get; set; }
+        public PostCategory Category { get; set; }
+        public PostLocation Location { get; set; }
 		public List<string> Images { get; set; }
 		public int AuthorId { get; set; }
 	}
