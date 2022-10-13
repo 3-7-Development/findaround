254eae264c4da3f91f3bcbd9ff2e95aaa5cc623d
gal0wsky <maciej.gawlowski.biznes@gmail.com>
2022-10-13 22:50:45 +0200

    Adding Data caching, configuring server access in .json --> 13.10.2022

---
+++ findaround/Configuration/ServerConnectionConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace findaround.Configuration
+{
+	public class ServerConnectionConfig
+	{
+		public string Emulator { get; set; }
+		public string Remote { get; set; }
+		public string Port { get; set; }
+	}
+}
+
---
+++ findaround/Utilities/BackendUtilities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using findaround.Configuration;
+using System.Reflection;
+using Newtonsoft.Json;
+using MonkeyCache.FileStore;
+
+namespace findaround.Utilities
+{
+	public static class BackendUtilities
+	{
+		public static string GetBaseServerUrl()
+		{
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			string json = string.Empty;
+
+			using (var stream = assembly.GetManifestResourceStream("findaround.Configuration.ServerConfig.json"))
+			{
+				using (var reader = new StreamReader(stream))
+				{
+					json = reader.ReadToEnd();
+				}
+			}
+
+			var config = JsonConvert.DeserializeObject<ServerConnectionConfig>(json);
+
+			var url = config.Remote + config.Port;
+
+			return url;
+		}
+
+		public static void SetToken(string token)
+		{
+			if (!string.IsNullOrWhiteSpace(token))
+				Barrel.Current.Add("UserToken", token, TimeSpan.FromDays(14));
+		}
+
+		public static string GetToken()
+		{
+			return Barrel.Current.Get<string>("UserToken");
+		}
+	}
+}
+
