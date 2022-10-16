11eb952d733ced403ae71e0bbfef29c4ffd42cfd
gal0wsky <maciej.gawlowski.biznes@gmail.com>
2022-10-16 13:16:37 +0200

    Changing colors, adding ButtonBackgroundColor property in ViewModelBase --> 16.10.2022

--- findaround/ViewModels/ViewModelBase.cs
+++ findaround/ViewModels/ViewModelBase.cs
@@ -7,8 +7,39 @@ namespace findaround.ViewModels
 {
 	public partial class ViewModelBase : BaseViewModel
 	{
+        bool isBusy;
+        public new bool IsBusy
+        {
+            get
+            {
+                return isBusy;
+            }
+            set
+            {
+                if (SetProperty(ref isBusy, value, "IsBusy"))
+                {
+                    IsNotBusy = !isBusy;
+                }
+                ButtonBackgroundColor = Colors.GreenYellow;
+            }
+        }
+
+        Color buttonBackgroundColor;
+        public Color ButtonBackgroundColor
+        {
+            get => buttonBackgroundColor;
+
+            set
+            {
+                var newColorName = IsBusy ? "ButtonUnavailableColor" : "Primary";
+                Application.Current.Resources.TryGetValue(newColorName, out var newColor);
+                SetProperty(ref buttonBackgroundColor, (Color)newColor);
+            }
+        }
+
 		public ViewModelBase()
 		{
+            ButtonBackgroundColor = Colors.GreenYellow;
 		}
 
         [RelayCommand]
