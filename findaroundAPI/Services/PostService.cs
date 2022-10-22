32cb61c06decf9c778c43b08454f00c6927459b9
gal0wsky <maciej.gawlowski.biznes@gmail.com>
2022-10-23 00:33:56 +0200

    Modyfying server --> 23.10.2022

--- findaroundAPI/Services/PostService.cs
+++ findaroundAPI/Services/PostService.cs
@@ -363,7 +363,9 @@ namespace findaroundAPI.Services
                 var postLocation = new GeoCoordinate(post.Latitude, post.Longitude);
                 var startSearchLocation = new GeoCoordinate(dto.Location.Latitude, dto.Location.Longitude);
 
-                var distanceFromUser = postLocation.GetDistanceTo(startSearchLocation);
+                //var distanceFromUser = postLocation.GetDistanceTo(startSearchLocation);
+
+                var distanceFromUser = GetDistance(dto.Location.Longitude, dto.Location.Latitude, post.Longitude, post.Latitude);
 
                 if (distanceFromUser <= dto.Distance)
                 {
@@ -375,18 +377,29 @@ namespace findaroundAPI.Services
             return new Result<IEnumerable<Post>>(postsList);
         }
 
-        private double GetPostDistance(PostLocation p1, PostLocation p2)
+        private static double GetDistance(double longitude, double latitude, double otherLongitude, double otherLatitude)
         {
-            var d1 = p1.Latitude * (Math.PI / 180.00);
-            var d2 = p2.Latitude * (Math.PI / 180.00);
-
-            var num1 = p1.Longitude * (Math.PI / 180.00);
-            var num2 = p2.Longitude * (Math.PI / 180.00);
-
+            var d1 = latitude * (Math.PI / 180.0);
+            var num1 = longitude * (Math.PI / 180.0);
+            var d2 = otherLatitude * (Math.PI / 180.0);
+            var num2 = otherLongitude * (Math.PI / 180.0) - num1;
             var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
 
             return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
         }
+
+        //private double GetPostDistance(PostLocation p1, PostLocation p2)
+        //{
+        //    var d1 = p1.Latitude * (Math.PI / 180.00);
+        //    var d2 = p2.Latitude * (Math.PI / 180.00);
+
+        //    var num1 = p1.Longitude * (Math.PI / 180.00);
+        //    var num2 = p2.Longitude * (Math.PI / 180.00);
+
+        //    var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
+
+        //    return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+        //}
     }
 }
 
